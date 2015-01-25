a4d06baf598f4b1b39bb1b03abaee0a8eeaa8a93
tryharder <maiorov.prog@gmail.com>
2015-01-25 15:33:34 +0200

    implementation of QuickUnion

--- src/main/java/chapt1/QuickUnion.java
+++ src/main/java/chapt1/QuickUnion.java
@@ -1,4 +1,36 @@
 package chapt1;
 
-public class QuickUnion {
+public class QuickUnion implements UF {
+    private int[] id;
+
+    public QuickUnion(int length) {
+        id = new int[length];
+        for (int i = 0; i < id.length; i++) {
+
+        }
+    }
+
+
+    @Override
+    public void union(int p, int q) {
+        id[p] = id[q];
+    }
+
+    @Override
+    public boolean connected(int p, int q) {
+
+        return root(p) == root(q);
+    }
+
+    private int root(int p) {
+        if (p == id[p]) {
+            return id[p];
+        }
+        return root(id[p]);
+    }
+
+    @Override
+    public int find(int p) {
+        return 0;
+    }
 }
